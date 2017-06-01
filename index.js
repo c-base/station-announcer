7bfb758a2f141c95138383cbbccc2c4947b49772
Henri Bergius <henri.bergius@iki.fi>
2017-06-01 19:20:47 +0200

    Use the production graph

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var noflo = require('noflo');
-exports.announcer = noflo.asCallback('station-announcer/Announcer', {
+exports.announcer = noflo.asCallback('station-announcer/StationAnnouncer', {
   baseDir: __dirname
 });
 exports.shout = noflo.asCallback('station-announcer/Shout', {
